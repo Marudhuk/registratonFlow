9c7f1b4cfa36c953f785fc86d679356f7b952c0a
Marudhuk <marudhu2001@gmail.com>
2023-06-17 00:30:54 +0530

    changes in code

--- src/app/forgotpass/forgotpass.component.ts
+++ src/app/forgotpass/forgotpass.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
+import { RegisterService } from '../register.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-forgotpass',
@@ -7,14 +9,24 @@ import { FormBuilder, Validators } from '@angular/forms';
   styleUrls: ['./forgotpass.component.css']
 })
 export class ForgotpassComponent {
-  constructor(private fb:FormBuilder) { }
+  constructor(private fb:FormBuilder,private service:RegisterService,private router:Router) { }
 
   reactiveForm=this.fb.group({
     email:[,[Validators.required,Validators.email]]
   })
 
+  ngOnInit() {
+
+  }
+
   onSubmit(){
-    console.log(this.reactiveForm.value);
+    this.service.forgotPassword(this.reactiveForm.value).subscribe((data:any)=>{
+      if(data.status){
+        console.log(data.message);
+        this.router.navigate(['verifyForgotPass']);
+        // localStorage.setItem('verifyForgotPass',this.reactiveForm.value.email);
+      }
+    })
   }
 
 }
