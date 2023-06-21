9c08e9c70c82ea639fa35287ec36fbd4f38dd04e
Marudhuk <marudhu2001@gmail.com>
2023-06-21 13:08:56 +0530

    completed registration flow

--- signup-server.js
+++ signup-server.js
@@ -63,7 +63,7 @@ async function sendMail(email, subject, text, html) {
             to: `${email}`,
             subject: `${subject}`,
             text: `${text}`,
-            html: `  ${html} `
+            html: `${html}`
         };
 
 
@@ -78,63 +78,16 @@ async function sendMail(email, subject, text, html) {
     })
 }
 
-
-
-//post method for signup
-// app.post('/signUp', async (req, res) => {
-//     let { name, email, password } = req.body;
-//     let sql = 'select * from usertable where email = ?';
-//     db.query(sql, [email], (err, result) => {
-//         if (err) {
-//             throw err;
-//         }
-//         else if (result.length) {
-//             console.log("res length > 1");
-//             if (result?.[0]?.isVerified == 0) {
-//                 let token = jwt.sign({ name, email }, secretKey);
-//                 console.log("verified =0");
-//                 // sendMail(email, token)
-//             } else {
-//                 res.json("Something went wrong");
-//             }
-//         } else {
-//             bcrypt.hash(password, saltRounds, async (err, hash) => {
-//                 if (err) {
-//                     console.log(err);
-//                 } else {
-//                     let resultForEmail = await sendEmail(email, token);
-//                     // console.log(hash,"insert");
-//                     if (resultForEmail) {
-//                         let sql = "insert into usertable (username,email,password,token) values (?,?,?,?)";
-//                         db.query(sql, [username, email, hash, token], (err, result) => {
-//                             if (err) {
-//                                 console.log(err);
-//                             } else {
-//                                 console.log(result);
-//                                 res.json("User Verified");
-//                             }
-//                         })
-//                     } else {
-//                         res.json("Something went wrong");
-//                     }
-//                 }
-//             })
-//         }
-
-//     });
-// });
-
-
 app.post('/signUp', (req, res) => {
     let { username, email } = req.body
     let text = "Please verify your email address."
     let subject = "Email Verification"
 
-    db.query("select id,username,email,token,isVerified,isActive from usertable where email=?", [req.body.email], async (err, result) => {
+    db.query("select id,username,email,token,isVerified,isActive from usertable where email=?", [email], async (err, result) => {
 
         if (err) throw err;
         else if (result.length) {
-            if (result?.[0].isVerified == 1) {
+            if (result?.[0]?.isVerified == 1) {
                 return res.json({
                     message: "Something went wrong",
                     status: false
@@ -142,7 +95,7 @@ app.post('/signUp', (req, res) => {
             } else {
                 let html = ` <h2>Email Verification</h2>
         <p>Thank you for signing up! To complete your registration, please verify your email address by clicking the link below:</p>
-        <a href="http://localhost:4200/verify/${result?.[0].token}" target="_blank">Verify Email</a>
+        <a href="http://localhost:4200/verify/${result?.[0]?.token}" target="_blank">Verify Email</a>
         <p>If you didn't sign up for an account, you can safely ignore this email.</p>
         <p>Best regards,<br>Your Name</p>`
                 const mailStatus = sendMail(result?.[0].email, subject, text, html);
@@ -189,18 +142,16 @@ app.post('/signUp', (req, res) => {
 })
 
 app.put('/verify/:token', (req, res) => {
-    let token = req.params.token
-    console.log(token, "token");
-    if (token) {
-        db.query("select email from usertable where token=?", [token], (err, result) => {
+    let ptoken = req.params.token
+    if (ptoken) {
+        db.query("select email,token from usertable where token=?", [ptoken], (err, result) => {
             if (err) throw err
             else if (result.length) {
                 let email = result?.[0].email
-                db.query("update usertable set isVerified=?,token=? where email=?", [1, null, email], (err, result) => {
+                let token = result?.[0].token
+                db.query("update usertable set isVerified=? where email=?", [1, email], (err, result) => {
                     if (err) throw err
                     else {
-                        let token = jwt.sign({ email }, secretKey);
-                        console.log(token, "token");
                         return res.status(200).json({
                             message: "User verified successfully",
                             data: token,
@@ -218,63 +169,61 @@ app.put('/verify/:token', (req, res) => {
 })
 
 app.put('/addPassword', (req, res) => {
-    console.log(req.body, "body");
     let { password, token } = req.body
-    if(token){
-    bcrypt.hash(password, saltRounds, async (err, hash) => {
-        if (err) {
-            return res.json({
-                message: "Something went wrong"
-            })
-        } else {
-            console.log(token, "token");
-            jwt.verify(token, secretKey, async (err, decode) => {
-                if (err) {
-                    return res.json({
-                        message: "Token expired"
-                    })
-                }
-                else {
-                    let email = decode.email
-                    console.log(email, "email");
-                    db.query("update usertable set password=? where email=? and isVerified=?", [hash, email, 1], (err, result) => {
-                        if (err) throw err
-                        else {
-                            return res.status(200).json({
-                                message: "Account created successfully",
-                                status: true
-                            })
-                        }
-                    })
-                }
-            })
-        }
-    })
-}
+    if (token) {
+        bcrypt.hash(password, saltRounds, async (err, hash) => {
+            if (err) {
+                return res.json({
+                    message: "Something went wrong"
+                })
+            }
+            else {
+                // console.log(token, "token");
+                // console.log(email, "email");
+                db.query("update usertable set password=?,token=? where token=? and isVerified=?", [hash, null, token, true], (err, result) => {
+                    if (err) throw err
+                    else {
+                        return res.status(200).json({
+                            message: "Account created successfully",
+                            status: true
+                        })
+                    }
+                })
+            }
+        })
+    } else {
+        return res.status(200).json({
+            message: "Something went wrong",
+            status: false
+        })
+    }
+
 })
 
-app.get('/login', (req, res) => {
+app.post('/login', (req, res) => {
     let { email, password } = req.body
-    db.query("select password from usertable where email=? and isVerified=1", [email], (err, result) => {
+    db.query("select email,password from usertable where email=? and isVerified=true", [email], (err, result) => {
         if (err) throw err
         else if (result.length) {
+            let token = jwt.sign({ email }, secretKey);
             bcrypt.compare(password, result?.[0]?.password, (err, result) => {
                 if (err) {
                     console.log(err);
                 } else if (result) {
-                    res.status(200).json({
+                    return res.status(200).json({
                         message: "Login successfully",
+                        data: token,
                         status: true
                     })
                 } else {
-                    res.status(200).json({
+                    return res.status(200).json({
                         message: "Something went wrong",
                         status: false
                     })
                 }
             })
         } else {
-            res.status(200).json({
+            return res.status(200).json({
                 message: "Something went wrong",
                 status: false
             })
@@ -283,30 +232,13 @@ app.get('/login', (req, res) => {
 })
 
 
-// var mailOptions = {
-//     from: "Your Name <your-email@example.com>",
-//     to: email,
-//     subject: "Password Reset",
-//     html: `
-//       <h2>Forgot Password</h2>
-//       <p>Hello,</p>
-//       <p>We received a request to reset your password. If you did not initiate this request, please ignore this email.</p>
-//       <p>To reset your password, click on the link below:</p>
-//       <p><a href="https://yourwebsite.com/reset-password?token=reset_token" target="_blank">Reset Password</a></p>
-//       <p>If you're having trouble clicking the link, you can copy and paste the following URL into your browser:</p>
-//       <p>https://yourwebsite.com/reset-password?token=reset_token</p>
-//       <p>This password reset link will expire in 24 hours for security reasons.</p>
-//       <p>Thank you,</p>
-//       <p>Your Company Name</p>
-//     `
-//   };
-
 
 app.post('/forgotPassword', (req, res) => {
     let { email } = req.body;
     let subject = "Password Reset"
-    let text = "Please click on the link to reset your password"
+    let text = "Please click on the link to reset your password";
     db.query("select id,email,token,isVerified from usertable where email=? and isVerified=?", [email, 1], (err, result) => {
+        console.log(result, "resultdddd");
         if (err) throw err
         else if (result.length) {
             let token = jwt.sign({ email }, secretKey);
@@ -316,9 +248,10 @@ app.post('/forgotPassword', (req, res) => {
             <p>Hello,</p>
             <p>We received a request to reset your password. If you did not initiate this request, please ignore this email.</p>
             <p>To reset your password, click on the link below:</p>
-            <p><a href="https://localhost:4200/reset-password?token=${token}" target="_blank">Reset Password</a></p>
-            <p>If you're having trouble clicking the link, you can copy and paste the following URL into your browser:</p>
-            <p>https://yourwebsite.com/reset-password?token=reset_token</p>
+            <p><a href="http://localhost:4200/verifyPassToken/${token}" target="_blank">Reset Password</a></p>
+            <p>If you're having trouble clicking the link, you can copy and paste the following URL into your browser:
+            </p>
+            <p>http://localhost:4200/verifyPassToken/${token}</p>
             <p>This password reset link will expire in 24 hours for security reasons.</p>
             <p>Thank you,</p>
             <p>Your Company Name</p>
@@ -328,19 +261,23 @@ app.post('/forgotPassword', (req, res) => {
                 if (err) throw err
                 else {
                     const mailStatus = sendMail(email, subject, text, html);
-                    if (mailStatus) return res.json({
-                        message: "Mail send successfuly",
-                    });
-                    return res.json({
-                        message: "something went wrong",
-                        status: mailStatus
-                    })
+                    if (mailStatus) {
+                        return res.json({
+                            message: "Mail send successfulyy",
+                            status: mailStatus
+                        });
+                    } else {
+                        return res.json({
+                            message: "something went wrong",
+                            status: mailStatus
+                        })
+                    }
                 }
             })
         }
         else {
             return res.json({
-                message: "Something went wrong",
+                message: "Mail send successfulyy",
                 status: false
             })
         }
@@ -349,33 +286,48 @@ app.post('/forgotPassword', (req, res) => {
 
 })
 
-app.get('/verifyPassToken/:token', (req, res) => {
+app.put('/verifyPassToken/:token', (req, res) => {
     let token = req.params.token
-    db.query("select email,isVerified from forgotpassword where token=?", [token], (err, result) => {
-        if (err) throw err
-        else if (result.length) {
-            let email = result?.[0].email
-            db.query("update forgotpassword set isVerified=?,token=? where email=?", [1, null, email], (err, result) => {
-
-                if (err) throw err
-                else {
-                    jwt.sign({ email }, secretKey, (err, token) => {
-                        if (err) throw err
-                        else {
-
-                            return res.json({
-                                message: "User verified successfully",
-                                status: true,
-                                data: token
-                            })
-                        }
-                    });
-                }
-            })
-
-        } else {
+    jwt.verify(token, secretKey, (err, decode) => {
+        console.log(decode, "decodeeeeeeee")
+        if (err) {
             return res.json({
-                message: "Something went wrongs"
+                message: "Something went wrong",
+                status: false
+            })
+        } else {
+            db.query(`SELECT token
+                        FROM forgotpassword
+                        WHERE email=?
+                        ORDER BY id DESC LIMIT 1`, 
+            [decode.email], (err, result) => {
+                console.log("Result",result);
+                if (err) throw err
+                else if (result.length) {
+                    let token = result?.[0]?.token
+                    db.query("update forgotpassword set isVerified=?,token=? where token=?", [true, null, token], (err, result) => {
+
+                        if (err) throw errtoken
+                        else {
+                            jwt.sign({ email : decode.email }, secretKey, (err, token) => {
+                                if (err) throw err
+                                else {
+
+                                    return res.json({
+                                        message: "User verified successfully",
+                                        status: true,
+                                        data: token
+                                    })
+                                }
+                            });
+                        }
+                    })
+
+                } else {
+                    return res.json({
+                        message: "Something went wrongs"
+                    })
+                }
             })
         }
     })
@@ -383,16 +335,17 @@ app.get('/verifyPassToken/:token', (req, res) => {
 
 app.put('/resetPassword', (req, res) => {
     let { password, token } = req.body
+    if(password && token){
     bcrypt.hash(password, saltRounds, async (err, hash) => {
         console.log(hash, "hash");
         if (err) {
             console.log(err);
         } else {
-            let email = jwt.verify(token, secretKey, (err, decode) => {
+            jwt.verify(token, secretKey, (err, decode) => {
                 if (err) throw err
                 else {
                     console.log(decode, "email");
-                    db.query("update usertable set password=? where email=? and isVerified=?", [hash, email, 1], (err, result) => {
+                    db.query("update usertable set password=? where email=? and isVerified=?", [hash, decode.email, 1], (err, result) => {
                         if (err) throw err
                         else if (result.affectedRows) {
                             res.status(200).json({
@@ -412,6 +365,12 @@ app.put('/resetPassword', (req, res) => {
 
         }
     })
+    }else{
+        res.status(200).json({
+            message: "Something went wrong",
+            status: false
+        })
+    }
 })
 
 
